22bd04975c1b668aa5119e70f60dcf6b23d2aa0c
Saurab Gautam <missionprojects250@gmail.com>
2026-06-12 16:11:33 +0545

    07-setup tailwind css and daisy ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,12 @@ function App() {
 
   return (
     <>
-      <h1>Welcome to the app</h1>
 
       <header>
+        <h1 className='text-red-700'>Welcome to the app</h1>
+        <button className='btn btn-primary btn-xl'>Click here</button>
+        <button className='btn btn-secondary btn-xs'>Click here</button>
+
         <Show when="signed-out">
           <SignInButton mode="modal" >login </SignInButton>
           <SignUpButton mode="modal" >Sign up</SignUpButton>

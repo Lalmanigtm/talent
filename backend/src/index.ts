a218b1edb4d699a0822cb7ff604630af8ba1cd2e
Saurab Gautam <missionprojects250@gmail.com>
2026-06-13 20:45:23 +0545

    08-setup navbar and start if main

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,61 +1,28 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import path from "path";
-import cors from "cors"
-import { serve } from "inngest/express"
-import { clerkMiddleware } from "@clerk/express";
 
 import { ENV } from "./lib/env";
-import { connectDB } from "./lib/db";
-import { functions, inngest } from "./lib/inngest";
-import { protectRoute } from "./middleware/protectRoute";
-import chatRoutes from "./routes/chatRoute"
-import sessionRoutes from "./routes/sessionRoute"
-
 
 const app = express();
 
-// middleware
-app.use(express.json())
-// credentials: true meaning ?? => server allows a browser to include cookies on request
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
+const __dirname = path.resolve();
 
-// adding clerk Middleware
-app.use(clerkMiddleware()) //this adds auth field to request object: req.auth()
-
-app.use("/api/inngest", serve({ client: inngest, functions }))
-app.use("/api/chat", chatRoutes)
-app.use("/api/sessions", sessionRoutes)
-
-app.get("/health", (req: Request, res: Response) => {
-  // req.auth
-  res.status(200).json({ message: "Welcome to the Health page" });
+app.get("/home", (req, res) => {
+  res.status(200).json({ msg: "Welcome to the home page" });
 });
-
-// when you pass an array of middleware to Express, it automatically flattens and executes them sequentially , one by one.
-app.get("/video-calls", protectRoute, (req: Request, res: Response) => {
-  res.status(200).json({ message: "this is the videocall endpoint" });
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "Welcome to the Health page" });
 });
-
 // Make our app ready for Deployment
 if (ENV.NODE_ENV === "production") {
-  // Go up two levels from backend/src to reach the root, then into frontend/dist
-  const frontendDistPath = path.join(__dirname, "../../frontend/dist");
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.use(express.static(frontendDistPath));
-
-  // In Express 5, use a Regex literal to catch all remaining frontend routes
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(frontendDistPath, "index.html"));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
   });
 }
 
-const startServer = async () => {
-  try {
-    await connectDB()
-    app.listen(ENV.PORT, () => console.log("Successfully connected to db at port:", ENV.PORT))
-  } catch (error) {
-    console.error("Error starting the server", error)
-  }
-}
-
-startServer()
+// const port = 3001;
+app.listen(ENV.PORT, () =>
+  console.log(`Succefully connected to db at port ${ENV.PORT}`),
+);
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,21 +1,73 @@
-import React from 'react'
+// import React from 'react'
+// import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+// import toast from "react-hot-toast"
 
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import toast from "react-hot-toast"
+// function HomePage() {
+//     return (
+//         <div>
+//             <button className='btn btn-secondary' onClick={() => toast.success("This is a success toast")}>Click</button>
+//             <header>
+//                 <Show when="signed-out">
+//                     <SignInButton mode="modal" />
+//                     <SignUpButton />
+//                 </Show>
+//                 <Show when="signed-in">
+//                     <UserButton />
+//                 </Show>
+//             </header>
+//         </div>
+//     )
+// }
+// export default HomePage
+
+// import React from 'react'
+import { Link } from 'react-router'
+import { ArrowRightIcon, SparkleIcon, ZapIcon } from "lucide-react"
+import { SignInButton } from '@clerk/react'
 
 function HomePage() {
     return (
-        <div>
-            <button className='btn btn-secondary' onClick={() => toast.success("This is a success toast")}>Click</button>
-            <header>
-                <Show when="signed-out">
-                    <SignInButton mode="modal" />
-                    <SignUpButton />
-                </Show>
-                <Show when="signed-in">
-                    <UserButton />
-                </Show>
-            </header>
+        <div className='bg-linear-to-br from-base-100 via-base-200 to-base-300'>
+            {/* Navbar */}
+            <nav className='bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-lg'>
+                <div className='max-w-7xl  mx-auto p-4 flex items-center justify-between'>
+                    {/* logo */}
+                    <Link to={"/"} className='flex items-center gap-3 hover:scale-105 transition-transform duration-200'>
+                        <div className='size-10 rounded-xl bg-linear-to-br from-primary via-secondary to-accent flex items-center justify-center shadow-lg'>
+                            <SparkleIcon className='size-6 text-white' />
+                        </div>
+
+                        <div className='flex flex-col'>
+                            <span className='font-white text-xl bg-linear-to-br from-primary via-secondary to-accent bg-clip-text text-transparent font-mono tracking-wider'>Talent-Interview</span>
+                            <span className='text-xs text-base-content/60 font-medium -mt-1'>Best Interview</span>
+                        </div>
+                    </Link>
+
+                    {/* auth Button */}
+                    <SignInButton mode="modal" >
+                        <button className='group px-6 py-3 bg-linear-to-br from-primary to-secondary rounded-xl text-white font-semibold text-sm shadow-lg hover:shadow-xl transition-all duration-200 hover:scale-105 flex items-center gap-3'>
+                            <span>Get Started</span>
+                            <ArrowRightIcon className='size-4 group-hover:translate-x-0.5 transition-transform' />
+                        </button>
+                    </SignInButton>
+
+                </div>
+
+            </nav>
+
+            {/* Hero section */}
+            <div className='max-w-7xl mx-auto px-4 py-20'>
+                <div className='grid lg:grid-cols-2 gap-12 items-center'>
+                    {/* left content */}
+                    <div className='sapce-y-8'>
+                        <div className='badge badge-primary badge-lg'>
+                            <ZapIcon className='size-4' />
+                            Real time colloboration
+                        </div>
+                    </div>
+                    {/* right image */}
+                </div>
+            </div>
         </div>
     )
 }
